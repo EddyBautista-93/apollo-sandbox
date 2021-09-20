17d745eec57fbdd415fc47d985baedd55102d37b
EddyBautista <bautista.j.eddy@gmail.com>
2021-09-20 06:31:22 -0500

    add new exhange rate component

--- src/App.js
+++ src/App.js
@@ -14,28 +14,63 @@ const client = new ApolloClient({
   cache: new InMemoryCache() // client uses to cache query results after fetching them 
 });
 
+// defining the query 
+const EXCHANGE_RATE = gql`
+query GetExchangeRates {
+  rates(currency: "USD") {
+    currency
+    rate
+  }
+}
+`;
+
+//exchange rates component 
+const ExchangeRates = () => {
+  const { loading, error, data } = useQuery(EXCHANGE_RATE); // this is where we pass in the query 
+
+  // apollo tracks a queries state with loading and error props
+  if(loading) return <p>Loading...</p>;
+  if(error) return <p>Error</p>
+
+  // results returned back are attached to the data property 
+  return data.map(( { currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
+
 
 function App() {
   
-  client
-  .query({
-    query: gql`
-      query GetRates {
-        rates(currency: "USD") {
-          currency
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
+    // testing out calls to API
+  // client
+  // .query({
+  //   query: gql`
+  //     query GetRates {
+  //       rates(currency: "USD") {
+  //         currency
+  //       }
+  //     }
+  //   `
+  // })
+  // .then(result => console.log(result));
 
   return (
     <div className="App">
-      <h1> Place Holder Text</h1>
+      <h1> My first Apollo app 🚀</h1>
       
       
     </div>
   );
 }
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
 
 export default App;
